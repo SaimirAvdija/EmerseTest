d280c134357ceac846a4f97eaee66f93c4abf367
Saimir avdija <katundari_@hotmail.com>
2021-10-24 15:14:00 +0200

    Add project files.

---
+++ EmerseTest/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace EmerseTest.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [Range(1,int.MaxValue,ErrorMessage ="Must be greater than 0")]
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
